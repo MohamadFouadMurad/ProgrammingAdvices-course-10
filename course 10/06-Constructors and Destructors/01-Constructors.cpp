cb4e76b7f6bb824f40f51f6ab38126ca651f7025
Mohamad Fouad Murad <foadmurad022@gmail.com>
2026-09-04 14:39:40 +0300

    Add files about Costructors and destructors

---
+++ course 10/06-Constructors and Destructors/01-Constructors.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+
+using namespace std;
+
+class  clsAddress
+{
+private:
+	string _AddressLine1;
+	string _AddressLine2;
+	string _POPBox;
+	string _ZipCode;
+
+public:
+	
+	clsAddress(string AddressLine1,string AddressLine2 ,string POPBox , string ZipCode)
+	{
+		_AddressLine1 = AddressLine1;
+		_AddressLine2 = AddressLine2;
+		_POPBox = POPBox;
+		_ZipCode = ZipCode;
+	}
+
+	void SetAddressLine1(string AddressLine1)
+	{
+		_AddressLine1 = AddressLine1;
+	}
+
+	string AddressLine1()
+	{
+		return _AddressLine1;
+	}
+
+	void SetAddressLine2(string AddressLine2)
+	{
+		_AddressLine2 = AddressLine2;
+	}
+
+	string AddressLine2()
+	{
+		return _AddressLine2;
+	}
+
+	void SetPOBox(string POBox)
+	{
+		_POPBox = POBox;
+	}
+
+	string POPBox()
+	{
+		return _POPBox;
+	}
+
+	void SetZipCode(string ZipCode)
+	{
+		_ZipCode = ZipCode;
+	}
+
+	string ZipCode()
+	{
+		return _ZipCode;
+	}
+
+	void Print()
+	{
+		cout << "\nAddress Details:\n";
+		cout << "------------------------" << endl;
+		cout << "AddressLine1  : " << _AddressLine1 << endl;
+		cout << "AddressLine2  : " << _AddressLine2 << endl;
+		cout << "POBox         : " << _POPBox << endl;
+		cout << "ZipCode       : " << _ZipCode << endl;
+	}
+
+};
+
+
+int main()
+{
+	clsAddress Address("Prince mohamad bin salman" , "B 303" , "11234" , "5555");
+
+	Address.Print();
+
+	system("pause>0");
+	return 0;
+
+}
