6a78765a12db973b023623d942682d96a018c927
Mohamad Fouad Murad <foadmurad022@gmail.com>
2026-09-05 22:43:47 +0300

    Add  OOP examples: inheritance, constructors, override , and Solve Exercises.

---
+++ course 10/09-inheritance/02-Parameterized Constructor of the base class.cpp
@@ -0,0 +1,196 @@
+#include <iostream>
+
+using namespace std;
+
+
+class clsPerson
+{
+private:
+	int _ID = 10;
+	string _FirstName = "";
+	string _LastName = "";
+	string _Email = "";
+	string _Phone = "";
+
+public:
+
+	clsPerson()
+	{
+
+	}
+
+	clsPerson(int ID, string FirstName, string LastName, string Email, string Phone)
+	{
+		_ID = ID;
+		_FirstName = FirstName;
+		_LastName = LastName;
+		_Email = Email;
+		_Phone = Phone;
+	}
+
+
+	//read only property
+	int ID()
+	{
+		return _ID;
+	}
+
+	//property set
+	void SetFirstName(string FirstName)
+	{
+		_FirstName = FirstName;
+	}
+
+
+	//property get
+	string FirstName()
+	{
+		return _FirstName;
+	}
+
+	//property set
+	void SetLastName(string LastName)
+	{
+		_LastName = LastName;
+	}
+
+	//property get
+	string LastName()
+	{
+		return _LastName;
+	}
+
+	//property get
+	string FullName()
+	{
+		return  _FirstName + " " + _LastName;
+	}
+
+	//property set
+	void SetEmail(string Email)
+	{
+		_Email = Email;
+	}
+
+	//property get
+	string Email()
+	{
+		return _Email;
+	}
+
+	//property set
+	void SetPhone(string Phone)
+	{
+		_Phone = Phone;
+	}
+
+	//property get
+	string Phone()
+	{
+		return _Phone;
+	}
+
+
+
+	void Print()
+	{
+		cout << "\nInfo: " << endl;
+		cout << "-------------------------------" << endl;
+		cout << "ID               :" << _ID << endl;
+		cout << "FirstName        :" << _FirstName << endl;
+		cout << "LastName         :" << _LastName << endl;
+		cout << "FullName         :" << FullName() << endl;
+		cout << "Email            :" << _Email << endl;
+		cout << "Phone            :" << _Phone << endl;
+		cout << "-------------------------------" << endl;
+	}
+
+
+	void SendEmail(string Subject, string Body)
+	{
+		cout << "\nthe Following message sent successfully to email: " << _Email << endl;
+		cout << "Subject: " << Subject << endl;
+		cout << "Body: " << Body << endl;
+	}
+
+
+	void SendSMS(string TextMessage)
+	{
+		cout << "\nthe following message sent successfully to phone: " << _Phone << endl;
+		cout << TextMessage << endl;
+	}
+
+};
+
+
+
+class clsEmployee : public clsPerson
+{
+
+private:
+
+	string _Title;
+	string _Department;
+	float _Salary;
+
+public:
+
+
+	clsEmployee(int ID, string FirstName, string LastName, string Email, string Phone,string Title,string Department,float Salary) 
+	:clsPerson(ID,FirstName,LastName,Email,Phone)
+	{
+		_Title = Title;
+		_Department = Department;
+		_Salary = Salary;
+	}
+
+	//Property Set
+	void SetTitle(string Title)
+	{
+		_Title = Title;
+	}
+
+	//Property Get
+	string Title()
+	{
+		return _Title;
+	}
+
+	//Property Set
+	void SetDepartment(string Department)
+	{
+		_Department = Department;
+	}
+
+	//Property Get
+	string Department()
+	{
+		return _Department;
+	}
+
+	//Property Set
+	void SetSalary(float Salary)
+	{
+		_Salary = Salary;
+	}
+
+	//Property Get
+	float Salary()
+	{
+		return _Salary;
+	}
+
+};
+
+int main()
+{
+	clsEmployee Employee1(10, "Mohamad", "Murad","A@a.com","8298982","Developer","ProgrammingAdvices",5000);
+
+	Employee1.Print();
+	cout << "\n" << Employee1.Title() << endl;
+	cout << "\n" << Employee1.Department() << endl;
+	cout << "\n" << Employee1.Salary() << endl;
+
+
+	return 0;
+}
