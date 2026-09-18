e901ee44e732cc8cac5bf1c3576ff72e37ff1a34
Mohamad Fouad Murad <foadmurad022@gmail.com>
2026-09-18 21:38:55 +0300

    Add clsDate library and finish second project

---
+++ course 10/017-Project/02-Date library.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "../clsDate.h"
+
+using namespace std;
+
+int main()
+{
+	clsDate Date1;
+	Date1.Print();
+
+	clsDate Date2("31/1/2022");
+	Date2.Print();
+
+	clsDate Date3(20, 12, 2022);
+	Date3.Print();
+
+	clsDate Date4(250, 2022);
+	Date4.Print();
+
+	return 0;
+}
